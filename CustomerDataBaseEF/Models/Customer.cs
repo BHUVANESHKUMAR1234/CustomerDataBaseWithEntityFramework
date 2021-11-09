16f0d442d2529c0a44c85433c147eb3cb36fb0f1
bbhuv <bbhuv@LAPTOP-J5EE0GD7>
2021-11-09 19:30:37 +0530

    Add project files.

---
+++ CustomerDataBaseEF/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CustomerDataBaseEF.Models
+{
+   
+
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        
+        [Column(TypeName ="nvarchar(200)")]
+        [Required(ErrorMessage ="This field is required.")]
+        [DisplayName("Customer Name")]
+        public string CustomerName { get; set; }
+        
+        [Column(TypeName = "int")]
+        public int Age { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        [DisplayName("Email ID")]
+        public string EmailId { get; set; }
+
+
+    }
+}
---
+++ CustomerDataBaseEF/Models/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerDataBaseEF.Models
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
